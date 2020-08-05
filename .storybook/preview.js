e562803194e9a69017f176bc57da22bce840dbad
Cesar Napoleon Mejia <cesar@prompt.com>
2020-08-05 12:44:55 -0700

    snapshots

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { addDecorator } from '@storybook/react'
 import { Center } from '../src/components/Center/Center'
+import '../src/index.css'
 
 addDecorator((story) => {
   return <Center>{story()}</Center>
